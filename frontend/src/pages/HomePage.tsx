e3de7eacd7f9b0b3852190a07bfff3966f5ce490
jongmin0216 <codejomo99@gmail.com>
2026-05-25 14:59:33 +0900

    Front Merge CI/CD Test

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ function HomePage() {
     <main className="min-h-screen bg-gray-50 flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-green-700">노닐강원 🏔️</h1>
-        <p className="mt-4 text-gray-500">강원도 관광 정보 서비스 </p>
+        <p className="mt-4 text-gray-500">강원도 관광 정보 서비스 - 졸리다 </p>
       </div>
     </main>
   )
